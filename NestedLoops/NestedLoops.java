5d48f4da6c9ae4aa2a3599beee3a784d9fa5108a
friedchik <jeremychik20042014@gmail.com>
2023-07-29 16:12:08 +1000

    created new file

---
+++ NestedLoops/NestedLoops.java
@@ -0,0 +1,11 @@
+package NestedLoops;
+
+public class NestedLoops {
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 3; i++) {
+            System.out.println("run: " + i);
+        }
+    }
+    
+}
